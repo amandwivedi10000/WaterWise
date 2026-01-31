c3e9553c4d373c7ccd6d89a860b26519371680f0
Aman Dwivedi <g25.aman.krishnadwivedi@gnkhalsa.edu.in>
2026-01-31 09:42:33 +0530

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@all-in-one-water-manager/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
